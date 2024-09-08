2e024cc4b42f82761255079c797b8e67d75a61f0
Punga911 <punga9111@gmail.com>
2024-09-08 12:10:20 +0300

    Fuel consumption added

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from race import Race
 
 class Driver():
-    def __init__(self, name, rating, team) -> None:
+    def __init__(self, name, rating, team, consistency) -> None:
         self.rating = rating
         self.name = name
         self.gapToCarInFront = 0.2
         self.team = team
         self.ovrTime = 0
+        self.consistency = consistency #0.1-1.0
+        self.lapsOfFuelRemaining = 0
 
 class Team():
     def __init__(self, name, performance) -> None:
@@ -15,62 +17,62 @@ class Team():
 
 #REDBULL
 redBull = Team("Redbull Racing", 90)
-verstappen = Driver("Max Verstappen", 99, redBull)
-perez = Driver("Sergio Perez", 66, redBull)
+verstappen = Driver("Max Verstappen", 99, redBull, 0.9)
+perez = Driver("Sergio Perez", 73, redBull, 0.8)
 
 
 #MCLAREN
 mclaren = Team("Mclaren", 99)
-norris = Driver("Lando Norris", 90, mclaren)
-piastri = Driver("Oscar Piastri", 88, mclaren)
+norris = Driver("Lando Norris", 90, mclaren, 0.8)
+piastri = Driver("Oscar Piastri", 88, mclaren, 0.8)
 
 
 #Ferrari
 ferrari = Team("Ferrari", 92)
-leclerc = Driver("Charles Leclerc", 93, ferrari)
-sainz = Driver("Carlos Sainz", 85, ferrari)
+leclerc = Driver("Charles Leclerc", 93, ferrari, 0.7)
+sainz = Driver("Carlos Sainz", 85, ferrari, 0.8)
 
 
 #Mercedes
 mercedes = Team("Mercedes", 90)
-hamilton = Driver("Lewis Hamilton", 93, mercedes)
-russell = Driver("Geroge Russell", 91, mercedes)
+hamilton = Driver("Lewis Hamilton", 93, mercedes, 0.8)
+russell = Driver("Geroge Russell", 91, mercedes, 0.7)
 
 
 #Aston Martin
-astonMartin = Team("Aston Martin", 50)
-alonso = Driver("Fernando Alonso", 80, astonMartin)
-stroll = Driver("Lance Stroll", 56, astonMartin)
+astonMartin = Team("Aston Martin", 45)
+alonso = Driver("Fernando Alonso", 80, astonMartin, 0.7)
+stroll = Driver("Lance Stroll", 56, astonMartin, 0.4)
 
 
 #Williams
 williams = Team("Williams", 44)
-albon = Driver("Alexander Albon", 52, williams)
-colapinto = Driver("Franco Colapinto", 4, williams)
+albon = Driver("Alexander Albon", 52, williams, 0.8)
+colapinto = Driver("Franco Colapinto", 4, williams, 0.5)
 
 
 #Haas 
 haas = Team("Haas", 34)
-magnussen = Driver("Kevin Magnussen", 48, haas)
-hulkenberg = Driver("Nico Hülkenberg", 64, haas)
+magnussen = Driver("Kevin Magnussen", 48, haas, 0.1)
+hulkenberg = Driver("Nico Hülkenberg", 64, haas, 0.4)
 
 
 #Alpine
 alpine = Team("Alpine", 34)
-gasly = Driver("Pierre Gasly", 60, alpine)
-ocon = Driver("Esteban Ocon", 60, alpine)
+gasly = Driver("Pierre Gasly", 60, alpine, 0.5)
+ocon = Driver("Esteban Ocon", 60, alpine, 0.5)
 
 
 #RB
 vcarb = Team("VCARB", 30)
-tsunoda = Driver("Yuki Tsunoda", 40, vcarb)
-ricciardo = Driver("Daniel Ricciardo", 40, vcarb)
+tsunoda = Driver("Yuki Tsunoda", 40, vcarb, 0.1)
+ricciardo = Driver("Daniel Ricciardo", 40, vcarb, 0.1)
 
 
 #Sauber
 sauber = Team("Sauber", 1)
-bottas = Driver("Valtteri Bottas", 20, sauber)
-zhou = Driver("Guanyu Zhou", 1, sauber)
+bottas = Driver("Valtteri Bottas", 20, sauber, 0.5)
+zhou = Driver("Guanyu Zhou", 1, sauber, 0.5)
 
 
 teams = [redBull, mclaren, ferrari, mercedes, astonMartin, williams, haas, alpine, vcarb, sauber]
--- race.py
+++ race.py
@@ -1,19 +1,24 @@
 import random
 
 class Race():
-    def __init__(self, name, laps, averageLapTimeInSecs) -> None:
+    def __init__(self, name : str, laps : int, averageLapTimeInSecs : int) -> None:
         self.name = name
         self.laps = laps
         self.avrgLapTime = averageLapTimeInSecs
     def calculateLapTime(self, driver, team):
-        lapTime = self.avrgLapTime - ((driver.rating/100) * 2 * random.uniform(-0.25, 0.25)) - ((team.performance / 100) * 2)
+        lapTime = self.avrgLapTime - ((driver.rating/100) * 2) - random.uniform(-(1-driver.consistency), 1-driver.consistency) - ((team.performance / 100) * 2) + driver.lapsOfFuelRemaining * 0.05
         driver.ovrTime += lapTime
+        driver.lapsOfFuelRemaining -= 1
         return f"1:{lapTime-60:.5}"
     def simulateRace(self, drivers):
         for i in range(1, self.laps + 1):
-            print(f"Lap {i}")
+
+            print(f"Lap {i}/{self.laps}")
+
             driverIndex = 0
             for driver in drivers:
+                if i == 1:
+                    driver.lapsOfFuelRemaining = self.laps
                 x = self.calculateLapTime(driver, driver.team)
 
                 if driverIndex != 0:
